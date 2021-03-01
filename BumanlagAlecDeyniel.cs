817dc0bf1b997b194a08641a5ad310bf20138034
Bumanlag-AlecDeyniel <79513490+Bumanlag-AlecDeyniel@users.noreply.github.com>
2021-03-01 18:10:28 +0800

    Add files via upload

---
+++ BumanlagAlecDeyniel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sample3_InputMyName
+{
+    class BumanlagAlecDeyniel
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your Name: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("\nHello {0}!!!",name);
+            Console.WriteLine("Welcome to OOP Environment.");
+        }
+    }
+}
